edd7440a50458464beeb186ee980588597ba624f
edwar <edwar@10.104.213.227>
2020-02-15 22:19:49 -0500

    nig

--- src/GameScreen.java
+++ src/GameScreen.java
@@ -1,5 +1,22 @@
+package gui;
 
+import javafx.scene.layout.*;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 
-public class GameScreen {
-
+public class GameScreen extends BorderPane {
+	private VBox infoArea;
+	private HBox buttonArea;
+	private Text title;
+	
+	public GameScreen() {
+		infoArea = new VBox(10);
+		
+		title = new Text("Game");
+		title.setFont(new Font("Verdana", 14));
+		
+		buttonArea = new HBox(10);
+		
+		getChildren().addAll(infoArea, buttonArea);
+	}
 }
--- src/WelcomeScreen.java
+++ src/WelcomeScreen.java
@@ -1,5 +1,19 @@
+package gui;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class WelcomeScreen {
+public class WelcomeScreen extends Application {
+	public void start(Stage primaryStage) throws Exception {
+		GameScreen root = new GameScreen();
 
+		primaryStage.setTitle("Game");
+		primaryStage.setScene(new Scene(root, 1000, 650));
+		primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
